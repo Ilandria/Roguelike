17cccfb874dd71caa90c8cb4385a78bd082c63e4
Charlotte C. Brown <charlotte.clover.brown@gmail.com>
2020-07-01 17:32:14 -0400

    Cleaned up vision mesh so it's no longer as shimmery. Lots of fog of war progress.

--- Assets/Scripts/VisionMesh.cs
+++ Assets/Scripts/VisionMesh.cs
@@ -19,6 +19,7 @@ namespace CCB.Roguelike
 
 		// Todo: Move this into PlayerCharacter.
 		[SerializeField]
+		[Range(0.0f, 360.0f)]
 		private float fov = 220.0f;
 
 		// Todo: Move this into PlayerCharacter.
@@ -29,37 +30,40 @@ namespace CCB.Roguelike
 		[SerializeField]
 		private float minViewDistance = 0.5f;
 
-		private readonly float deg2Rad = Mathf.PI / 180.0f;
 		private Mesh visionMesh = null;
 		private Vector3[] vertices = null;
 		private Vector2[] uv = null;
 		private int[] indices = null;
 		private RaycastHit2D[] rayHitArray = new RaycastHit2D[1] { new RaycastHit2D() };
 		private int solidObjectLayer = -1;
-		private float angleIncrease = 0.0f;
-		private Vector2 rayHeading = Vector2.zero;
-		private Vector3 localRayVector = Vector3.zero;
 		private WaitForSeconds visionUpdateWait = null;
+		private Vector2[] rayHeadings = null;
+		private float[] rayAngles = null;
 		
 		public void OnEnable()
 		{
 			visionMesh = new Mesh();
 			GetComponent<MeshFilter>().mesh = visionMesh;
-
 			solidObjectLayer = LayerMask.GetMask("Solid Objects"); // Todo: Don't hardcode this.
 
-			angleIncrease = Mathf.PI * 2.0f / rayCount;
 			int numVertices = rayCount + 1; // +1 to account for the center vertex.
 			vertices = new Vector3[numVertices];
 			uv = new Vector2[numVertices];
 			indices = new int[rayCount * 3];
 
+			float angleIncrease = Mathf.PI * 2.0f / rayCount;
+			rayHeadings = new Vector2[rayCount];
+			rayAngles = new float[rayCount];
+
 			for (int ray = 0; ray < rayCount; ray++)
 			{
 				int tri = ray * 3;
 				indices[tri + 0] = 0;
-				indices[tri + 1] = ray + 1;
-				indices[tri + 2] = ray + 2 < numVertices ? ray + 2 : 1;
+				indices[tri + 2] = ray + 1;
+				indices[tri + 1] = ray + 2 < numVertices ? ray + 2 : 1;
+
+				rayAngles[ray] = ray * angleIncrease;
+				rayHeadings[ray] = new Vector2(Mathf.Cos(rayAngles[ray]), Mathf.Sin(rayAngles[ray]));
 			}
 
 			visionMesh.SetVertices(vertices);
@@ -74,34 +78,25 @@ namespace CCB.Roguelike
 		{
 			while (isActiveAndEnabled)
 			{
-				// Turns Vector3.Angle into a 0-360 range to properly offset vision cone.
-				float lookAngle = Vector3.Angle(Vector3.right, playerCharacter.LookDirection);
-				if (Vector3.Dot(Vector3.up, playerCharacter.LookDirection) < 0.0f)
-				{
-					lookAngle = 360.0f - lookAngle;
-				}
-
-				Vector3 worldOrigin = visionSource.position;
 				float solidObjectViewFactor = 1.0f - 1.0f / (minViewDistance + 1.0f); // Todo: Don't hardcode this. Approaches 1 at x => inf., 0.5 at x = 1.
-				float fovRadians = fov * deg2Rad;
-				float halfFovRadians = fovRadians / 2.0f;
-				float angle = halfFovRadians + lookAngle * deg2Rad;
+				Vector3 worldOrigin = visionSource.position;
 				Vector3 localOrigin = transform.InverseTransformPoint(worldOrigin);
 				vertices[0] = localOrigin;
-				float totalAngle = 0.0f;
 
 				for (int i = 1; i <= rayCount; i++)
 				{
-					rayHeading.Set(Mathf.Cos(angle), Mathf.Sin(angle));
-					localRayVector = transform.InverseTransformDirection(rayHeading);
+					Vector2 rayHeading = rayHeadings[i - 1];
 
 					// We can see farther in the forward view arc.
-					float unobstructedViewDistance = totalAngle <= fovRadians ? viewDistance : minViewDistance;
+					float viewAlignment = Vector3.Dot(rayHeading, playerCharacter.LookDirection) * 0.5f + 0.5f;
+					float fovEdgeLocation = 1.0f - (fov / 360.0f);
+					viewAlignment = 1.0f - Mathf.Clamp01((fovEdgeLocation - viewAlignment) / fovEdgeLocation);
+					float unobstructedViewDistance = Mathf.Lerp(minViewDistance, viewDistance, Mathf.Pow(viewAlignment, 10.0f));
 
 					// Vision ray hit nothing.
 					if (Physics2D.RaycastNonAlloc(worldOrigin, rayHeading, rayHitArray, unobstructedViewDistance, solidObjectLayer) == 0)
 					{
-						vertices[i] = localOrigin + localRayVector * unobstructedViewDistance;
+						vertices[i] = localOrigin + (Vector3)rayHeading * unobstructedViewDistance;
 					}
 					// Vision ray hit something.
 					else
@@ -111,11 +106,8 @@ namespace CCB.Roguelike
 						// Distance from the player to the maximum depth within the solid object that the player could see.
 						float obstructedViewDistance = Vector3.Distance(rayHitArray[0].point + rayHeading * solidObjectViewFactor, worldOrigin);
 						// To prevent seeing through walls, either use the effective view distance or the point within the solid object, whichever is shorter.
-						vertices[i] = localOrigin + localRayVector * Mathf.Min(unobstructedViewDistance, obstructedViewDistance);
+						vertices[i] = localOrigin + (Vector3)rayHeading * Mathf.Min(unobstructedViewDistance, obstructedViewDistance);
 					}
-
-					totalAngle += angleIncrease;
-					angle -= angleIncrease;
 				}
 
 				visionMesh.SetVertices(vertices);
